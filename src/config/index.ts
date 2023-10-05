a4a8f3d3bf2badbbcdbeddaa763f0d852a3b31e4
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-05 09:06:11 +0600

    name updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,8 +9,8 @@ export default {
   auth_token: process.env.AUTH_TOKEN,
   auth_token_expires_in: process.env.AUTH_TOKEN_EXPIRE_IN,
   refresh_token: process.env.REFRESH_TOKEN,
-  refresh_token_expires_in : process.env.REFRESH_TOKEN_EEPIRE_IN,
-  aws_access_key: process.env.ACCESS_KEY,
+  refresh_token_expires_in : process.env.REFRESH_TOKEN_EXPIRE_IN,
+  aws_access_key: process.env.AWS_ACCESS_CREDENTIAL,
   aws_secret_key: process.env.AWS_SECRET_ACCESS_KEY,
   files_bucket_name: process.env.FILES_BUCKET_NAME,
   aws_s3_region: process.env.AWS_S3_REGION
